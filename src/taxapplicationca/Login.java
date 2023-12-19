b6a7f6e53768aa7ceac03211271d0f5af8b76ca2
2022256 <2022256@student.cct.ie>
2023-12-19 18:23:04 +0000

    Create Login.java
    create class login and imports for it

---
+++ src/taxapplicationca/Login.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taxapplicationca;
+
+/**
+ *
+ * @author 4istik
+ */
+public class Login {
+    
+}
