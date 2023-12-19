040587776f8e9fc94b82fcdc8166222e492c94c1
IvanPopoff <2022180@student.cct.ie>
2023-12-19 17:16:37 +0000

    Created TaxCalculator
    taxCalculation interface

---
+++ src/taxapplicationca/TaxCalulator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package taxapplicationca;
+
+/**
+ *
+ * @author ivanp
+ */
+public interface TaxCalulator {
+    
+    // Interface named TaxCalculator defining tax calculation methodspublic interface TaxCalculator {
+        // Method to calculate overall tax based on gross income and tax credits
+    double calculateTax(double grossIncome, double taxCredits);    
+    // Method to calculate Pay As You Earn (PAYE) tax based on gross income    
+    double calculatePAYE(double grossIncome);
+        // Method to calculate Universal Social Charge (USC) based on gross income
+    double calculateUSC(double grossIncome);    
+    // Method to calculate Pay Related Social Insurance (PRSI) based on gross income    
+    double calculatePRSI(double grossIncome);
+    }
+
