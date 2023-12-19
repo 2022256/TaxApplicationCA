17b954064ab59701d04414bef7bbcd4f47650590
2022256 <2022256@student.cct.ie>
2023-12-19 17:24:52 +0000

    fix
    fix

--- src/Calculation/TaxCalculator.java
+++ src/Calculation/TaxCalculator.java
@@ -9,13 +9,11 @@ package Calculation;
  * @author ivanp & andreic
  */
 public interface TaxCalculator {
-    
-    // Interface named TaxCalculator defining tax calculation methodspublic interface TaxCalculator {
     // Method to calculate overall tax based on gross income and tax credits
     double calculateTax(double grossIncome, double taxCredits);    
     // Method to calculate Pay As You Earn (PAYE) tax based on gross income    
     double calculatePAYE(double grossIncome);
-        // Method to calculate Universal Social Charge (USC) based on gross income
+    // Method to calculate Universal Social Charge (USC) based on gross income
     double calculateUSC(double grossIncome);    
     // Method to calculate Pay Related Social Insurance (PRSI) based on gross income    
     double calculatePRSI(double grossIncome);
